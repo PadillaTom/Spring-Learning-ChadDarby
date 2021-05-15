9b018bb886f9815c0b935375d37abba2ab317326
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-15 10:42:11 +0200

    Implementing CustomerDAO OK

--- HibernateTutorial/Spring-Hibernate-CRUD/web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ HibernateTutorial/Spring-Hibernate-CRUD/web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,36 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// *** Injecting CustomerDAO: 
+	// Spring Will Scan for a component that IMPLEMENTS CustomerDAO interface (CustoemrDAOImpl) ***
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// Get Customers from DAO:
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// Add Customers to my Spring MVC Model:
+		theModel.addAttribute("customers", theCustomers); 
+		// Returns this "customers" and have to call it from JSP
+		
 		return "list-customers";
 	}
 }
