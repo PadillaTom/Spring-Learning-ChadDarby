4f54158ae5453dae853c492051b0ff3fe13c58b5
Tomas Padilla <padillatomasagustin@gmail.com>
2021-05-17 10:06:16 +0200

    Adding SERVICE Layer, Starting CRUD

--- HibernateTutorial/Spring-Hibernate-CRUD/web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ HibernateTutorial/Spring-Hibernate-CRUD/web-customer-tracker/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,27 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// *** Injecting CustomerDAO: 
-	// Spring Will Scan for a component that IMPLEMENTS CustomerDAO interface (CustoemrDAOImpl) ***
+	// *** Injecting Customer Service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	
-	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		// Get Customers from DAO:
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// Get Customers from SERVICE (which delegates to DAO):
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// Add Customers to my Spring MVC Model:
 		theModel.addAttribute("customers", theCustomers); 
